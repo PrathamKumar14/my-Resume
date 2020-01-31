a9e8f00f8a0218e70e147137266c25c6129f3d3f
Pratham kumar <prathamsolera@gmail.com>
2020-01-31 22:26:56 +0530

    Add typing effect

---
+++ typing.js
@@ -0,0 +1,25 @@
+
+
+var i = 0;
+var txt = "Hey there ! I am Pratham a Web Developer";
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+
+
+
+  function typeWriter() {
+
+    if (i < txt.length) {
+      if(i < 16) {
+        document.getElementById("effect1").innerHTML += txt.charAt(i);
+        i++;
+      } else if (i >= 16 && i < 24) {
+        document.getElementById("effect2").innerHTML += txt.charAt(i);
+        i++;
+      } else {
+        document.getElementById("effect3").innerHTML += txt.charAt(i);
+        i++;
+      }
+      setTimeout(typeWriter, speed);
+    }
+  }
