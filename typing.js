65561341de616570be7e495bdc77f01f8c0ad757
Pratham kumar <prathamsolera@gmail.com>
2020-02-07 20:19:15 +0530

    Add blinking cursor

--- typing.js
+++ typing.js
@@ -1,4 +1,4 @@
-
+//jshint esversion: 6
 
 var i = 0;
 var txt = "Hey there ! I am Pratham a Web Developer.";
@@ -7,19 +7,35 @@ var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 
 
-  function typeWriter() {
+function typeWriter() {
 
-    if (i < txt.length) {
-      if(i < 16) {
-        document.getElementById("effect1").innerHTML += txt.charAt(i);
-        i++;
-      } else if (i >= 16 && i < 24) {
-        document.getElementById("effect2").innerHTML += txt.charAt(i);
-        i++;
-      } else {
-        document.getElementById("effect3").innerHTML += txt.charAt(i);
-        i++;
-      }
-      setTimeout(typeWriter, speed);
+  if (i < txt.length) {
+    if (i < 16) {
+      document.getElementById("effect1").innerHTML += txt.charAt(i);
+      i++;
+    } else if (i >= 16 && i < 24) {
+      document.getElementById("effect2").innerHTML += txt.charAt(i);
+      i++;
+    } else {
+      document.getElementById("effect3").innerHTML += txt.charAt(i);
+      i++;
     }
+    setTimeout(typeWriter, speed);
   }
+
+}
+
+setInterval(() => {
+  setTimeout(function() {
+    if (cursor) {
+      document.getElementById('cursor').innerHTML = "|";
+      document.getElementById('cursor').style.opacity = 0;
+      cursor = false;
+    } else {
+      document.getElementById('cursor').innerHTML = "|";
+      document.getElementById('cursor').style.opacity = 1;
+      cursor = true;
+    }
+  }, 3900);
+
+}, 400);
